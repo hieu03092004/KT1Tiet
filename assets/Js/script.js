831ae7e5c8ec9790a8c33c0c6cca7604725fab92
hieu <hieu@example.com>
2024-04-25 04:01:34 +0700

    fix css and create layout by using js

---
+++ assets/Js/script.js
@@ -0,0 +1,32 @@
+var board;
+var playerO="O";
+var playerX="X";
+var currentPlayer=playerX;
+var gameOver=false;
+
+window.onload=function(){
+    startGame();
+}
+function startGame() {
+    board = [
+                [' ', ' ', ' '],
+                [' ', ' ', ' '],
+                [' ', ' ', ' ']
+            ]
+
+    for (let r = 0; r < 3; r++) {
+        for (let c = 0; c < 3; c++) {
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.classList.add("tile");
+            if (r == 0 || r == 1) {
+                tile.classList.add("horizontal-line");
+            }
+            if (c == 0 || c == 1) {
+                tile.classList.add("vertical-line");
+            }
+
+            document.getElementById("board").appendChild(tile);
+        }
+    }
+}
